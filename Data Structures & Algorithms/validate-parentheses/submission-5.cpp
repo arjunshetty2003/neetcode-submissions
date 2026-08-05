9ac6ebb9ce5ee5c8d6be9ea29255a8631a86ef87
Arjun Shetty <arjunshetty0312@gmail.com>
2026-08-05 11:32:13 +0530

    Add: validate-parentheses - submission-5

---
+++ Data Structures & Algorithms/validate-parentheses/submission-5.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+
+        for (char c : s) {
+            if (c == '(' || c == '{' || c == '[') {
+                st.push(c);
+            }
+            else {
+                if (st.empty()) {
+                    return false;
+                }
+                else {
+                    if ((st.top() == '(' && c != ')') || (st.top() == '[' && c != ']') || (st.top() == '{' && c != '}')) {
+                    return false;
+                }
+                st.pop();
+                }
+            }
+        }
+
+        return st.empty();
+    }
+};
